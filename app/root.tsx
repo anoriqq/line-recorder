9705cbf8e13688fd6c5893c78de054675af14b13
anoriqq <shota.yoshikawa@anoriqq.com>
2023-11-16 01:02:06 +0900

    feat: add minimum root.tsx

---
+++ app/root.tsx
@@ -0,0 +1,44 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  LiveReload,
+  MetaFunction,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Line Recorder" },
+    {
+      property: "og:title",
+      content: "Line Recorder",
+    },
+    {
+      name: "description",
+      content: "This app is line recorder",
+    },
+  ];
+};
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link
+          rel="icon"
+          href="data:image/x-icon;base64,AA"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Hello world!</h1>
+        <Outlet />
+
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
