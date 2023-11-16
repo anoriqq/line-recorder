cbee65cd1ccc57315030936f513ba281ee6c821b
anoriqq <shota.yoshikawa@anoriqq.com>
2023-11-16 23:51:43 +0900

    feat: 複数の音声を録音して並べるように

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,10 @@ import {
   LiveReload,
   MetaFunction,
 } from "@remix-run/react";
-import { useState } from "react";
+import {
+  useRef,
+  useState,
+} from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -21,16 +24,89 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-function Main() {
-  const [isRecording, setIsRecording] = useState(false);
+const constraints = {
+  audio: true,
+  video: false,
+}
 
-  const onClick = () => {
-    setIsRecording(!isRecording)
+function Main() {
+  const [stream, setStream] = useState<MediaStream|null>(null)
+
+  const handleGetMicPermission = async () => {
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+      setStream(stream)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  }
+
+  const mediaRecorderRef = useRef<MediaRecorder|null>(null)
+  const audioChunksRef = useRef<Blob[]>([])
+  const [audioURLs, setAudioURLs] = useState<string[]>([])
+
+  const dataavailableListener = (event: BlobEvent) => {
+      if (typeof event.data === 'undefined') {
+        return
+      }
+      if (event.data.size === 0) {
+        return
+      }
+      audioChunksRef.current.push(event.data)
+  }
+
+  const stopListener = () => {
+    const blob = new Blob(audioChunksRef.current)
+    audioChunksRef.current = []
+    const url = URL.createObjectURL(blob)
+    setAudioURLs((prev) => [...prev, url])
+  }
+
+  const handleStart = () => {
+    if (stream === null) {
+      return
+    }
+    if (mediaRecorderRef.current === null) {
+      mediaRecorderRef.current = new MediaRecorder(stream)
+      mediaRecorderRef.current.addEventListener('dataavailable', dataavailableListener)
+      mediaRecorderRef.current.addEventListener('stop', stopListener)
+    }
+    if (mediaRecorderRef.current.state === 'recording') {
+      return
+    }
+    mediaRecorderRef.current.start()
+  }
+
+  const handleStop = () => {
+    if (mediaRecorderRef.current === null) {
+      return
+    }
+    if (mediaRecorderRef.current.state === 'inactive') {
+      return
+    }
+    mediaRecorderRef.current.stop()
   }
 
   return (
     <main>
-      <button className="rec" onClick={onClick}> {isRecording ? 'Stop' : 'Rec'} </button>
+      <div>
+        {stream === null ? (
+          <button onClick={handleGetMicPermission}>Get Mic Permission</button>
+        ) : (
+          <>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+          </>
+        )}
+      </div>
+      <div>
+        { audioURLs.map((url, i) => (
+          <div key={i}>
+            <audio src={url} controls />
+          </div>
+        )) }
+      </div>
     </main>
   );
 }
